724d8415e4389a584580b9a0d23f05c86087fafc
anthony_vanadia <31926505+avanadia@users.noreply.github.com>
2018-03-06 11:12:39 -0500

    validators that update on state changes

--- Demo-Start-Updated/src/app/customers/customer.component.ts
+++ Demo-Start-Updated/src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -12,18 +12,43 @@ export class CustomerComponent implements OnInit {
     customerForm: FormGroup;
     customer: Customer= new Customer();
 
+    constructor(private fb: FormBuilder) {
+    }
+
     save() {
         console.log(this.customerForm);
         console.log('saved: ' + JSON.stringify(this.customerForm.value));        
     }
 
-    ngOnInit():void {
-        this.customerForm = new FormGroup({
-            firstName: new FormControl(),
-            lastName: new FormControl(),
-            email: new FormControl(),
-            sendCatalog: new FormControl(true),
+    populateTestData(): void {
+        // use patch the change some, set to change all
+        this.customerForm.patchValue({
+            firstName: 'Jack',
+            lastName: 'Harkness',
             
+            sendCatalog: false
         });
+    }
+
+    setNotification(notifyVia: string): void {
+        const phoneControl = this.customerForm.get('phone');
+        if (notifyVia === 'text') {
+            phoneControl.setValidators(Validators.required);
+        } else {
+            phoneControl.clearValidators();
+        }
+        phoneControl.updateValueAndValidity();
+    }
+
+    ngOnInit():void {
+
+        this.customerForm = this.fb.group({
+            firstName: ['', [Validators.required, Validators.minLength(3)]],
+            lastName: ['', [Validators.required, Validators.maxLength(50)]],
+            email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')]],
+            sendCatalog: false,
+            phone: '',
+            notification: 'email'
+        }); 
     }
  }
